8ea6b471aada8531f8b1053a50ccdf3aa4a2480b
Kevin Pulido <kevinp8001@gmail.com>
2022-07-21 23:15:06 -0500

    Version 1.1.0: Añadido el endpoint / con un mensaje de redireccionamiento a la documentación

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-_VERSION = '1.0.1'
+_VERSION = '1.1.0'
 
 app = FastAPI(title='API Challenge AB InBev',
               version=_VERSION)
@@ -9,5 +9,10 @@ app = FastAPI(title='API Challenge AB InBev',
 def health_msg():
     ''' Devuelve un mensaje de status predeterminado si y solo si la API está arriba. Endpoint de health para monitoreo.'''
     return {'status':'Service UP'}
+
+@app.get('/', status_code=200, tags=['Health'])
+def index():
+    '''Devuelve un mensaje predeterminado en el Endpoint raíz.'''
+    return {'MSG':'Para ver la documentación vaya a la url /redoc'}
 
 # uvicorn app:app
