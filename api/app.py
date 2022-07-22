14f43f9368e66bc769d7051557452637468f96cb
Kevin Pulido <kevinp8001@gmail.com>
2022-07-21 22:43:36 -0500

    cambios de forma [TAG y status_code]y descripción en el endpoint /health

--- api/app.py
+++ api/app.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
-_VERSION = '1.0.0'
+_VERSION = '1.0.1'
 
 app = FastAPI(title='API Challenge AB InBev',
               version=_VERSION)
 
-@app.get('/health')
+@app.get('/health', status_code=200, tags=['Health'])
 def health_msg():
+    ''' Devuelve un mensaje de status predeterminado si y solo si la API está arriba. Endpoint de health para monitoreo.'''
     return {'status':'Service UP'}
 
 # uvicorn app:app
