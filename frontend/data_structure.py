134c5a97b3711129f683a6cd8466ffa78606f639
Kevin Pulido <kevinp8001@gmail.com>
2022-07-24 00:53:42 -0500

    Mejoras en código para reducir duplicidad de código

--- frontend/data_structure.py
+++ frontend/data_structure.py
@@ -3,44 +3,10 @@ from pydantic import BaseModel
 class Tuits(BaseModel):
     textos: list[str] | None = []
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "textos": ["it's OK my friend", "That's bad!"]
-                }
-        }
-
 class Prediccion(BaseModel):
     tweet:str
     pred:int
     label:str
 
-    class Config:
-        schema_extra = {
-            "example":{
-                    "tweet": "I like this code!",
-                    "pred": 1,
-                    "label": "Positive"
-                    }
-        }
-
 class RespuestaModelo(BaseModel):
-
     respuesta: list[Prediccion]
-
-    class Config:
-        schema_extra = {
-            "example": 
-            [
-                {
-                    "tweet": "it's OK my friend",
-                    "pred": 1,
-                    "label": "Positive"
-                },
-                {
-                    "tweet": "That's bad!",
-                    "pred": 0,
-                    "label": "Negative"
-                }
-            ]
-        }
